9cb813af0878e71e56b82715d197d9254df166a2
Piottry <121669608+Piottry@users.noreply.github.com>
2023-01-13 18:58:18 +0100

    Add files via upload

---
+++ Central/central_robot/central_robot.ino
@@ -0,0 +1,79 @@
+/*
+  BLE_Peripheral.ino
+
+  This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE 
+  as a peripheral device and specifies a service and a characteristic. Depending 
+  of the value of the specified characteristic, an on-board LED gets on. 
+
+  The circuit:
+  - Arduino Nano 33 BLE. 
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBLE.h>
+      
+
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+
+
+
+BLEService gestureService(deviceServiceUuid); 
+BLEByteCharacteristic gestureCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite);
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);  
+  
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  digitalWrite(LEDR, HIGH);
+  digitalWrite(LEDG, HIGH);
+  digitalWrite(LEDB, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  
+  if (!BLE.begin()) {
+    Serial.println("- Starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("Arduino Nano 33 BLE (Peripheral)");
+  BLE.setAdvertisedService(gestureService);
+  gestureService.addCharacteristic(gestureCharacteristic);
+  BLE.addService(gestureService);
+  gestureCharacteristic.writeValue(-1);
+  BLE.advertise();
+
+  Serial.println("Nano 33 BLE (Peripheral Device)");
+  Serial.println(" ");
+}
+
+void loop() {
+  BLEDevice central = BLE.central();
+  Serial.println("- Discovering central device...");
+  delay(500);
+
+  if (central) {
+    Serial.println("* Connected to central device!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(central.address());
+    Serial.println(" ");
+
+    while (central.connected()) {
+      digitalWrite(LED_BUILTIN,LOW);
+      delay(2000);
+      digitalWrite(LED_BUILTIN,HIGH);
+      delay(2000);
+    }
+    Serial.println("* Disconnected to central device!");
+    
+  }
+  
+}
---
+++ Commande/Commande/Commande.ino
@@ -0,0 +1,93 @@
+/*
+  BLE_Central_Device.ino
+
+  This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE Sense 
+  as a central device and looks for a specified service and characteristic in a 
+  peripheral device. If the specified service and characteristic is found in a 
+  peripheral device, the last detected value of the on-board gesture sensor of 
+  the Nano 33 BLE Sense, the APDS9960, is written in the specified characteristic. 
+
+  The circuit:
+  - Arduino Nano 33 BLE Sense. 
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBLE.h>
+#include <Arduino_APDS9960.h>
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  
+  
+  if (!BLE.begin()) {
+    Serial.println("* Starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+  
+  BLE.setLocalName("Nano 33 BLE (Central)"); 
+  BLE.advertise();
+
+  Serial.println("Arduino Nano 33 BLE Sense (Central Device)");
+  Serial.println(" ");
+}
+
+void loop() {
+  connectToPeripheral();
+}
+
+void connectToPeripheral(){
+  BLEDevice peripheral;
+  
+  Serial.println("- Discovering peripheral device...");
+
+  do
+  {
+    BLE.scanForUuid(deviceServiceUuid);
+    peripheral = BLE.available();
+  } while (!peripheral);
+  
+  if (peripheral) {
+    Serial.println("* Peripheral device found!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(peripheral.address());
+    Serial.print("* Device name: ");
+    Serial.println(peripheral.localName());
+    Serial.print("* Advertised service UUID: ");
+    Serial.println(peripheral.advertisedServiceUuid());
+    Serial.println(" ");
+    BLE.stopScan();
+    controlPeripheral(peripheral);
+  }
+}
+
+void controlPeripheral(BLEDevice peripheral) {
+  Serial.println("- Connecting to peripheral device...");
+
+  if (peripheral.connect()) {
+    Serial.println("* Connected to peripheral device!");
+    Serial.println(" ");
+  } else {
+    Serial.println("* Connection to peripheral device failed!");
+    Serial.println(" ");
+    return;
+  }
+
+
+
+ 
+    
+  
+  while (peripheral.connected()) {
+
+
+  
+  }
+  Serial.println("- Peripheral device disconnected!");
+}
+  
